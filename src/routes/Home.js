1dcb268f72d7b4c8073f9ae2d38a6cdf17ccccc8
chang.xianglin <chang.xianglin@jyall.com>
2018-08-21 17:08:04 +0800

    RecPlaylist

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,17 +2,18 @@ import React from 'react';
 import {connect} from 'dva';
 import {routerRedux} from 'dva/router';
 import styles from './common/index.css';
-import s from './TopArtistList.css';
-
-const RECPLAYLIST = ({recPlaylist}) => {
-  const list = null;
-  list = recPlaylist.map((item,index) => {
+// import {playlistWrapper,ant_carousel,playlistCarousel,playlistItem, cover,mask,icon_play,name,playcount} from './Home.less';
+import './Home.less';
+import {Carousel} from 'antd'
+const RecPlaylist = ({dispatch, recPlaylist}) => {
+  let list = null;
+  list = recPlaylist.map((item, index) => {
     return (
-      <div>
+      <div className={playlistItem} key={index}>
         <div className={cover}>
           <img src={item.picUrl} alt=""/>
           <i className={mask}></i>
-          <i className={icon_play} onClick={()=>alert()}></i>
+          <i className={icon_play} onClick={()=>alert(item.picUrl)}></i>
         </div>
         <div className={name}>{item.name}</div>
         <div className={playcount}>{item.playCount}</div>
@@ -20,34 +21,55 @@ const RECPLAYLIST = ({recPlaylist}) => {
     )
   })
   return (
-    <div>
-
+    <div className={playlistWrapper}>
+      <Carousel effect="fade"
+                slidesToShow={5}
+                dots={true}
+                slidesToScroll={5}
+                autoplay={true}
+                className={ant_carousel}>
+        {
+          recPlaylist.map((item, index) => {
+            return (
+              <div className={playlistItem} key={index}>
+                <div className={cover}>
+                  <img src={item.picUrl} alt=""/>
+                  <i className={mask}></i>
+                  <i className={icon_play} onClick={()=>alert(item.picUrl)}></i>
+                </div>
+                <div className={name}>{item.name}</div>
+                <div className={playcount}>{item.playCount}</div>
+              </div>
+            )
+          })
+        }
+      </Carousel>
     </div>
   )
 }
 
-const RECNEWALBUM = ({recNewAlbum}) => {
+const RecNewAlbum = ({recNewAlbum}) => {
   return (
     <div>
 
     </div>
   )
 }
-const RECNEWSONG = ({recNewSong}) => {
+const RecNewSong = ({recNewSong}) => {
   return (
     <div>
 
     </div>
   )
 }
-const RECMV = ({recMv}) => {
+const RecMv = ({recMv}) => {
   return (
     <div>
 
     </div>
   )
 }
-const RECDJ = ({recDj}) => {
+const RecDj = ({recDj}) => {
   return (
     <div>
 
@@ -60,13 +82,15 @@ class Home extends React.Component{
     super(props);
   }
   componentDidMount(){
-
+    this.props.dispatch({
+      type:'music/fetchRecPlaylist'
+    })
   }
   render(){
     return (
       <>
         <div className={styles.container}>
-
+          <RecPlaylist recPlaylist={this.props.home.playlist}/>
         </div>
       </>
     )
