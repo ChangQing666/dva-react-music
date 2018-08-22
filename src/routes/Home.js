8456746945f322c2f848cb39d9b13d0453d17c5b
chang.xianglin <chang.xianglin@jyall.com>
2018-08-22 15:27:02 +0800

    RecPlaylist

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,31 +2,18 @@ import React from 'react';
 import {connect} from 'dva';
 import {routerRedux} from 'dva/router';
 import styles from './common/index.css';
-// import {playlistWrapper,ant_carousel,playlistCarousel,playlistItem, cover,mask,icon_play,name,playcount} from './Home.less';
-import './Home.less';
-import {Carousel} from 'antd'
+import {playlistWrapper,ant_carousel,playlistCarousel,playlistItem, cover,mask,icon_play,name,playcount} from './Home.less';
+import Slider from "react-slick";
 const RecPlaylist = ({dispatch, recPlaylist}) => {
-  let list = null;
-  list = recPlaylist.map((item, index) => {
-    return (
-      <div className={playlistItem} key={index}>
-        <div className={cover}>
-          <img src={item.picUrl} alt=""/>
-          <i className={mask}></i>
-          <i className={icon_play} onClick={()=>alert(item.picUrl)}></i>
-        </div>
-        <div className={name}>{item.name}</div>
-        <div className={playcount}>{item.playCount}</div>
-      </div>
-    )
-  })
   return (
     <div className={playlistWrapper}>
-      <Carousel effect="fade"
+      <Slider   dots={true}
+                arrows={true}
                 slidesToShow={5}
-                dots={true}
                 slidesToScroll={5}
+                infinite={true}
                 autoplay={true}
+                autoplaySpeed={5000}
                 className={ant_carousel}>
         {
           recPlaylist.map((item, index) => {
@@ -43,12 +30,13 @@ const RecPlaylist = ({dispatch, recPlaylist}) => {
             )
           })
         }
-      </Carousel>
+      </Slider>
     </div>
   )
 }
 
 const RecNewAlbum = ({recNewAlbum}) => {
+
   return (
     <div>
 
@@ -90,6 +78,7 @@ class Home extends React.Component{
     return (
       <>
         <div className={styles.container}>
+          <RecNewAlbum/>
           <RecPlaylist recPlaylist={this.props.home.playlist}/>
         </div>
       </>
