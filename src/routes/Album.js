618edbaf7ffe16ceee1ea1ab94f740451f74e7eb
chang.xianglin <chang.xianglin@jyall.com>
2018-08-30 10:50:23 +0800

    专辑简介

--- src/routes/Album.js
+++ src/routes/Album.js
@@ -30,9 +30,7 @@ const AlbumDesc = ({albumDesc, dispatch}) => {
         </p>
         <p>发行时间：{timestampToTime(albumDesc.publishTime)}</p>
         <p>发行公司：{albumDesc.company}</p>
-        <div>
-          专辑介绍： {albumDesc.description}
-        </div>
+
         <button className={`iconfont icon-bofang1 ${styles.btnPlayAll}`}
                 onClick={() => handlePlayAll(albumDesc.id)}> 播放全部
         </button>
@@ -46,9 +44,15 @@ class Album extends React.Component{
   constructor(props) {
     super(props);
     this.fetchAlbum = this.fetchAlbum.bind(this);
+    this.state={isShowMore: false}
   }
   fetchAlbum(id){
     this.props.dispatch({type: 'music/fetchAlbum', payload:id});
+  }
+  handleStretch(){
+    this.setState({
+      isShowMore:!this.state.isShowMore
+    })
   }
   componentDidMount() {
     const {id} = this.props.match.params;
@@ -63,7 +67,17 @@ class Album extends React.Component{
     return(
       <div className={styles.container}>
         <AlbumDesc albumDesc={this.props.album} dispatch={this.props.dispatch}/>
-        <Songlist  songlist={this.props.songs}  dispatch={this.props.dispatch}/>
+        <div  className={styles.listWrapper}>
+          <div className={styles.listContainer}>
+            <Songlist  songlist={this.props.songs}  dispatch={this.props.dispatch}/>
+
+          </div>
+          <div className={styles.descriptionContainer}>
+            <h5>简介</h5>
+            <p className={`${this.state.isShowMore?styles.showMore:''}`}>{this.props.album.description}</p>
+            <span onClick={()=>this.handleStretch()}>{this.state.isShowMore?'[收起]':'[展开]'}</span>
+          </div>
+        </div>
       </div>
     )
   }
